ba608b4072510539c3245dcdaff2bef53e6cec70
Sam Marrello <sammarrello@gmail.com>
2022-03-09 19:18:47 -0500

    completed part one of day one successfully, onto part two!

--- src/main/kotlin/dayOne.kt
+++ src/main/kotlin/dayOne.kt
@@ -6,5 +6,24 @@ fun dayOne() {
 }
 
 fun dayOnePart1() {
+
+    var increases = 0
+
     val depth = File("src/main/resources/inputDay1.txt")
+        .readLines()
+        .map(String::toInt)
+
+    for (i in depth.indices) {
+        if (depth[i] == 3) {
+            break
+        }
+        else if (depth[i] < depth[i + 1]) {
+            increases++
+        }
+    }
+    println(increases)
+}
+
+fun dayOnePart2() {
+
 }
